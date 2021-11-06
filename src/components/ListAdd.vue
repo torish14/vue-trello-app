09e98024557ee73a72c49002a17e4955186942f7
torish14 <nanamin.104@icloud.com>
2021-11-07 05:04:00 +0900

    ✨ feat: リストを追加するため、リストアドコンポーネントの作成

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form class="addList" @submit.prevent="addList">
+      <input
+        v-model="title"
+        type="text"
+        class="text-input"
+        placeholder="Add new list"
+      />
+      <button class="add-button" type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+    }
+  },
+  methods: {
+    addList() {
+      this.$store.dispatch('addList', {
+        title: this.title,
+      })
+      this.title = ''
+    },
+  },
+}
+</script>
