b16f6381172b2aedf6e6e8ea797f1a84e0faea2c
torish14 <nanamin.104@icloud.com>
2021-11-08 00:53:30 +0900

    ✨ feat: リストを表示するため、Listコンポーネントを作成

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <div class="listHeader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deleteList" @click="removeList">✕</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    listIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    removeList() {
+      if (confirm('本当にこのリストを削除しますか？')) {
+        this.$store.dispatch('removeList', {
+          listIndex: this.listIndex,
+        })
+      }
+    },
+  },
+}
+</script>
