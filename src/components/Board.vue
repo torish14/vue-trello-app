82d9841a28fa20628b57c8bd018800dcb524bc34
torish14 <nanamin.104@icloud.com>
2021-11-08 01:10:47 +0900

    ✨ feat: リストを表示するため、listコンポーネントを呼び出し

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
     <header>Vue Trello App</header>
+<<<<<<< Updated upstream
     <main><p class="info-line">All: 0 tasks</p></main>
+=======
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <div class="list-index">
+        <list
+          v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        ></list>
+        <ListAdd />
+      </div>
+    </main>
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
+<<<<<<< Updated upstream
 export default {}
+=======
+import ListAdd from './ListAdd'
+import List from './List'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    ListAdd,
+    List,
+  },
+  computed: {
+    ...mapState(['lists']),
+  },
+}
+>>>>>>> Stashed changes
 </script>
