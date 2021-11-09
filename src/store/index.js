c91e8cf3b3e4abd5ccb19f8e74595b11503efb86
torish14 <nanamin.104@icloud.com>
2021-11-10 00:59:38 +0900

    ✨ feat: リストコンポーネント、カードコンポーネントを削除するため、mutations, actions にメソッドを作成

--- src/store/index.js
+++ src/store/index.js
@@ -24,37 +24,55 @@ const store = new Vuex.Store({
           },
         ],
   },
-  getters: {},
+  getters: {
+    totalCardCount(state) {
+      let count = 0
+      state.lists.map((context) => (count += context.cards.length))
+      return count
+    },
+  },
   mutations: {
     addList(state, payload) {
       state.lists.push({ title: payload.title, cards: [] })
+    },
+    removeList(state, payload) {
+      state.lists.splice(payload.listIndex, 1)
+    },
+    addCardToList(state, payload) {
+      state.lists[payload.listIndex].cards.push({
+        body: payload.body,
+      })
+    },
+    removeCardFromList(state, payload) {
+      state.lists[payload.listIndex].cards.splice(
+        payload.listIndex,
+        1
+      )
+    },
+    updateList(state, payload) {
+      state.lists = payload.lists
     },
   },
   actions: {
     addList(context, payload) {
       context.commit('addList', payload)
     },
+    removeList(context, payload) {
+      context.commit('removeList', payload)
+    },
+    addCardToList(context, payload) {
+      context.commit('addCardToList', payload)
+    },
+    removeCardFromList(context, payload) {
+      context.commit('removeCardFromList', payload)
+    },
+    updateList(context, payload) {
+      context.commit('updateList', payload)
+    },
   },
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 })
 
-store.subscribe((mutation, state) => {
+store.subscribe((mutations, state) => {
   localStorage.setItem('trello-lists', JSON.stringify(state.lists))
 })
 
