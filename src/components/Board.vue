3a8ccf854da3bae214fb11c761a222def3a80169
torish14 <nanamin.104@icloud.com>
2021-11-11 11:03:13 +0900

    ✨ feat: コンポーネントをドラッグできるようにするため、Draggable の読み込み

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,32 +2,49 @@
   <div>
     <header>Vue Trello App</header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <div class="list-index">
-        <List
-          v-for="(item, index) in lists"
-          :key="item.id"
-          :title="item.title"
-          :listIndex="index"
-        />
-        <ListAdd />
+        <Draggable :list="lists" @end="movingList" class="list-index">
+          <List
+            v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :cards="item.cards"
+            :listIndex="index"
+            @change="movingCard"
+          />
+          <ListAdd />
+        </Draggable>
       </div>
     </main>
   </div>
 </template>
 
 <script>
+import Draggable from 'vuedraggable'
 import ListAdd from './ListAdd'
 import List from './List'
 import { mapState } from 'vuex'
 
 export default {
   components: {
+    Draggable,
     ListAdd,
     List,
   },
   computed: {
     ...mapState(['lists']),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    },
+  },
+  methods: {
+    movingCard() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
   },
 }
 </script>
